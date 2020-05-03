2c2e41a78ceb12c667c3952afd0c157c0096c549
Benjamin Staneck <bstaneck@manticoregames.com>
2020-05-03 04:37:50 +0200

    start working on getting API in

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,17 @@
 import * as vscode from 'vscode';
+import { EnumCompletionProvider } from "./enum"
+
+const SELECTOR = { scheme: "file", language: "lua" }
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let provider1 = vscode.languages.registerCompletionItemProvider('lua', {
+	let provider1 = vscode.languages.registerCompletionItemProvider(SELECTOR, {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
 			// a simple completion item which inserts `Hello World!`
 			const simpleCompletion = new vscode.CompletionItem('Hello World!');
 
-			// a completion item that inserts its text as snippet,
-			// the `insertText`-property is a `SnippetString` which will be
-			// honored by the editor.
-			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
-
-			// a completion item that can be accepted by a commit character,
-			// the `commitCharacters`-property is set which means that the completion will
-			// be inserted and then the character will be typed.
 			const commitCharacterCompletion = new vscode.CompletionItem('console');
 			commitCharacterCompletion.commitCharacters = ['.'];
 			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
@@ -35,34 +28,12 @@ export function activate(context: vscode.ExtensionContext) {
 			// return all completion items as array
 			return [
 				simpleCompletion,
-				snippetCompletion,
 				commitCharacterCompletion,
 				commandCompletion
 			];
 		}
 	});
 
-	const provider2 = vscode.languages.registerCompletionItemProvider(
-		'lua',
-		{
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-
-				// get all text until the `position` and check if it reads `console.`
-				// and if so then complete if `log`, `warn`, and `error`
-				let linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.endsWith('console.')) {
-					return undefined;
-				}
-
-				return [
-					new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
-				];
-			}
-		},
-		'.' // triggered whenever a '.' is being typed
-	);
-
-	context.subscriptions.push(provider1, provider2);
+	context.subscriptions.push(provider1);
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(SELECTOR, new EnumCompletionProvider(), "."))
 }
