15ac9d8f8f51a40b62983fb87fb1b66c182f0424
Benjamin Staneck <bstaneck@manticoregames.com>
2020-05-11 05:10:19 +0200

    add test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,11 @@
 import * as assert from 'assert';
+var expect = require('chai').expect;
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+import { getApiDump, ApiEnum } from "./../../api";
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -11,5 +13,11 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
 		assert.equal(-1, [1, 2, 3].indexOf(0));
+	});
+});
+
+suite("Expect API to exist", () => {
+	test('Is true?', () => {
+		expect(getApiDump).to.be.exist;
 	});
 });
